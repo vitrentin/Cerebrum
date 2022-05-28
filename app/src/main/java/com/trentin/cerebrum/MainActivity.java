ef7650965133174e16b6f3c3c028fe11e47aca03
Trentin <vtrentin15@gmail.com>
2022-05-27 22:13:15 -0300

    Layouts-Buttons-Cerebrum

--- app/src/main/java/com/trentin/cerebrum/MainActivity.java
+++ app/src/main/java/com/trentin/cerebrum/MainActivity.java
@@ -1,13 +1,34 @@
 package com.trentin.cerebrum;
 
+import android.content.Intent;
+import android.content.SharedPreferences;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
-public class MainActivity extends AppCompatActivity {
-
+public class MainActivity extends AppCompatActivity implements View.OnClickListener{
+    private Button play,exit;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        play = (Button) findViewById(R.id.jogar);
+        play.setOnClickListener(this);
+        exit = (Button) findViewById(R.id.sair);
+        exit.setOnClickListener(this);
+    }
+
+    @Override
+    public void onClick(View view) {
+        if (view == play) {
+            Intent i = new Intent(this, Tela1.class);
+            Bundle b = new Bundle();
+            i.putExtras(b);
+            startActivity(i);
+        }
+        if (view == exit) {
+            finishAffinity();
+        }
     }
 }
--- app/src/main/java/com/trentin/cerebrum/Tela6.java
+++ app/src/main/java/com/trentin/cerebrum/Tela6.java
@@ -1,13 +1,35 @@
 package com.trentin.cerebrum;
 
+import android.content.DialogInterface;
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
-public class Tela6 extends AppCompatActivity {
-
+public class Tela6 extends AppCompatActivity implements View.OnClickListener{
+    private Button menu, exit;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_tela6);
+        menu = (Button) findViewById(R.id.menu);
+        menu.setOnClickListener(this);
+        exit = (Button) findViewById(R.id.sair2);
+        exit.setOnClickListener(this);
     }
+
+    @Override
+    public void onClick(View view) {
+        if (view == menu) {
+            Intent i = new Intent(this, Tela1.class);
+            Bundle b = new Bundle();
+            i.putExtras(b);
+            startActivity(i);
+        }
+        if (view == exit) {
+            finishAffinity();
+        }
+    }
+
 }
